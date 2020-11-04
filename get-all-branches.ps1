7447ccf1a26a515a2b8b44381fb9a99ee76acf78
Gagneet Singh <gagneet@gmail.com>
2020-11-04 14:30:09 +1100

    Script to 'git pull' all branches of all repositories under the current folder

---
+++ get-all-branches.ps1
@@ -0,0 +1,31 @@
+param()
+ 
+function Get-AllRepos ()
+{
+    Get-ChildItem -Recurse -Depth 2 -Force |
+        Where-Object { $_.Mode -match "h" -and $_.FullName -like "*\.git" } |
+        ForEach-Object {
+            $dir = Get-Item (Join-Path $_.FullName "../")
+            Push-Location $dir
+ 
+            "Fetching $($dir.Name)"
+            function Get-AllRemoteBranches {
+			     Invoke-Expression "git branch -r"                       `
+			     | ForEach-Object { $_ -Match "origin\/(?'name'\S+)" }  `
+			     | ForEach-Object { Out-Null; $matches['name'] }        
+			}
+
+
+			function Get-AllBranches {
+			    Get-AllRemoteBranches `
+                    | ForEach-Object { Invoke-Expression "git checkout $_" } `
+                    | ForEach-Object { Invoke-Expression "git pull" }
+            }
+            
+            Get-AllBranches
+            
+            Pop-Location
+        }
+ }
+ 
+Get-AllRepos
